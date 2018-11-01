90c4dd6714afe5a5fd0a65edae0a614225a013f5
Sugandan RG <sg4940@nyu.edu>
2018-10-31 21:20:22 -0400

    config file

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import csv
 from mongoengine import *
-from csvload import *
+from csvload import * #model import
+from config import conf as config #mongodb instance connection details
 
-loadData = [{'id': int, 'data': int, 'FactorConfigId': int, 'createdAt': str}]
-csv_file = csv.DictReader(open("testData.csv"))
+loadData = []
+csvFile = csv.DictReader(open("testData.csv"))
 #dataset = [i for i in csv_file if i['data'] > 0  i['data'] != '' ]
-for d in map(dict, csv_file):
+#{'id': int, 'data': int, 'FactorConfigId': int, 'createdAt': str}  //if int(i['data']) > 0 and int(i['data']) != ''
+for d in map(dict, csvFile):
     loadData.append(d)
 
-print(loadData)
+#validData = [i for i in loadData i]
 
-connect(host='mongodb+srv://sugandanrg:changednow!@cluster0-hd2ly.mongodb.net/coindesk')
+for i in loadData:
+    if int(i['data']) > 0 and int(i['data']) != '':
+        print(int(i['data']))
+
+connect(host='mongodb+srv://' + config['username'] + ':'
+                              + config['pwd'] + '@cluster0-hd2ly.mongodb.net/'
+                              + config['db'])
+
+print(coincaps.objects.count())
+print(len(validData))
